ca0a25143fced6d202afc445fb20a8db8ef9adb5
Matteo Paltenghi <mattepalte@live.it>
2024-11-18 16:17:14 +0100

    add/uncomment tests

--- tests/generators/knitting/test_base_knitter.py
+++ tests/generators/knitting/test_base_knitter.py
@@ -25,88 +25,88 @@ def qc_2():
     return qc
 
 
-# def test_combine_circuits_parallel(qc_1, qc_2):
-#     """
-#     Test that the combine_circuits method correctly combines two quantum circuits.
-#     """
-#     pk = ParallelKnitter(qc_1, qc_2)
-#     qc_combine = pk.combine_circuits()
+def test_combine_circuits_parallel(qc_1, qc_2):
+    """
+    Test that the combine_circuits method correctly combines two quantum circuits.
+    """
+    pk = ParallelKnitter(qc_1, qc_2)
+    qc_combine = pk.combine_circuits()
 
-#     qc_viz = pk.get_viz_circuit()
-#     assert qc_viz is not None, "The visualized circuit should not be None"
+    qc_viz = pk.get_viz_circuit()
+    assert qc_viz is not None, "The visualized circuit should not be None"
 
-#     displaytext = pk.get_viz_map_name_to_text()
-#     displaycolor = pk.get_viz_map_name_to_color()
+    displaytext = pk.get_viz_map_name_to_text()
+    displaycolor = pk.get_viz_map_name_to_color()
 
-#     expected_image = Path(
-#         'tests/artifacts/viz/combined_circuit_parallel.png')
+    expected_image = Path(
+        'tests/artifacts/viz/combined_circuit_parallel.png')
 
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         temp_output_path = Path(tmpdirname) / 'combined_circuit.png'
-#         qc_viz.draw("mpl",
-#                     style={
-#                         "displaytext": displaytext,
-#                         "displaycolor": displaycolor},
-#                     filename=temp_output_path)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        temp_output_path = Path(tmpdirname) / 'combined_circuit.png'
+        qc_viz.draw("mpl",
+                    style={
+                        "displaytext": displaytext,
+                        "displaycolor": displaycolor},
+                    filename=temp_output_path)
 
-#         assert filecmp.cmp(
-#             temp_output_path, expected_image), "The generated image does not match the expected image"
-
-
-# def test_combine_circuits_interleaved(qc_1, qc_2):
-#     """
-#     Test that the combine_circuits method correctly combines two quantum circuits using InterleavedKnitter.
-#     """
-#     ik = InterleavedKnitter(qc_1, qc_2)
-#     qc_combine_interleaved = ik.combine_circuits()
-
-#     qc_viz_interleaved = ik.get_viz_circuit()
-#     assert qc_viz_interleaved is not None, "The visualized circuit should not be None"
-
-#     displaytext_interleaved = ik.get_viz_map_name_to_text()
-#     displaycolor_interleaved = ik.get_viz_map_name_to_color()
-
-#     expected_image_interleaved = Path(
-#         'tests/artifacts/viz/combined_circuit_interleaved.png')
-
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         temp_output_path_interleaved = Path(
-#             tmpdirname) / 'combined_circuit_interleaved.png'
-#         qc_viz_interleaved.draw(
-#             "mpl",
-#             style={"displaytext": displaytext_interleaved,
-#                    "displaycolor": displaycolor_interleaved},
-#             filename=temp_output_path_interleaved)
-#         assert filecmp.cmp(temp_output_path_interleaved,
-#                            expected_image_interleaved), "The generated image does not match the expected image"
+        assert filecmp.cmp(
+            temp_output_path, expected_image), "The generated image does not match the expected image"
 
 
-# def test_combine_circuits_unitary(qc_1, qc_2):
-#     """
-#     Test that the combine_circuits method correctly combines two quantum circuits using UnitaryKnitter.
-#     """
-#     uk = UnitaryKnitter(qc_1, qc_2)
-#     qc_combine_unitary = uk.combine_circuits()
+def test_combine_circuits_interleaved(qc_1, qc_2):
+    """
+    Test that the combine_circuits method correctly combines two quantum circuits using InterleavedKnitter.
+    """
+    ik = InterleavedKnitter(qc_1, qc_2)
+    qc_combine_interleaved = ik.combine_circuits()
 
-#     qc_viz_unitary = uk.get_viz_circuit()
-#     assert qc_viz_unitary is not None, "The visualized circuit should not be None"
+    qc_viz_interleaved = ik.get_viz_circuit()
+    assert qc_viz_interleaved is not None, "The visualized circuit should not be None"
 
-#     displaytext_unitary = uk.get_viz_map_name_to_text()
-#     displaycolor_unitary = uk.get_viz_map_name_to_color()
+    displaytext_interleaved = ik.get_viz_map_name_to_text()
+    displaycolor_interleaved = ik.get_viz_map_name_to_color()
 
-#     expected_image_unitary = Path(
-#         'tests/artifacts/viz/combined_circuit_unitary.png')
+    expected_image_interleaved = Path(
+        'tests/artifacts/viz/combined_circuit_interleaved.png')
 
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         temp_output_path_unitary = Path(
-#             tmpdirname) / 'combined_circuit_unitary.png'
-#         qc_viz_unitary.draw(
-#             "mpl",
-#             style={"displaytext": displaytext_unitary,
-#                    "displaycolor": displaycolor_unitary},
-#             filename=temp_output_path_unitary)
-#         assert filecmp.cmp(temp_output_path_unitary,
-#                            expected_image_unitary), "The generated image does not match the expected image"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        temp_output_path_interleaved = Path(
+            tmpdirname) / 'combined_circuit_interleaved.png'
+        qc_viz_interleaved.draw(
+            "mpl",
+            style={"displaytext": displaytext_interleaved,
+                   "displaycolor": displaycolor_interleaved},
+            filename=temp_output_path_interleaved)
+        assert filecmp.cmp(temp_output_path_interleaved,
+                           expected_image_interleaved), "The generated image does not match the expected image"
+
+
+def test_combine_circuits_unitary(qc_1, qc_2):
+    """
+    Test that the combine_circuits method correctly combines two quantum circuits using UnitaryKnitter.
+    """
+    uk = UnitaryKnitter(qc_1, qc_2)
+    qc_combine_unitary = uk.combine_circuits()
+
+    qc_viz_unitary = uk.get_viz_circuit()
+    assert qc_viz_unitary is not None, "The visualized circuit should not be None"
+
+    displaytext_unitary = uk.get_viz_map_name_to_text()
+    displaycolor_unitary = uk.get_viz_map_name_to_color()
+
+    expected_image_unitary = Path(
+        'tests/artifacts/viz/combined_circuit_unitary.png')
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        temp_output_path_unitary = Path(
+            tmpdirname) / 'combined_circuit_unitary.png'
+        qc_viz_unitary.draw(
+            "mpl",
+            style={"displaytext": displaytext_unitary,
+                   "displaycolor": displaycolor_unitary},
+            filename=temp_output_path_unitary)
+        assert filecmp.cmp(temp_output_path_unitary,
+                           expected_image_unitary), "The generated image does not match the expected image"
 
 
 def test_combine_circuits_with_c_if():
